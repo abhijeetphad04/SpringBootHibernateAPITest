2a295c94252c1490d80c38960815b6f6e80a428a
shree <shree@shree-PC>
2020-06-11 15:31:16 +0530

    SpringBoothibernateApi commited

---
+++ SpringHibernateApi/src/main/java/com/tel/SpringHibernateApi/service/BridgeVOTO.java
@@ -0,0 +1,37 @@
+package com.tel.SpringHibernateApi.service;
+
+import org.springframework.stereotype.Service;
+
+import com.tel.SpringHibernateApi.model.Country;
+import com.tel.SpringHibernateApi.model.Employee;
+import com.tel.SpringHibernateApi.model.EmployeeUI;
+
+@Service
+public class BridgeVOTO {
+
+	public Employee convertEmployeeVOTO(EmployeeUI employeeUI) {
+		System.out.println("In Bridge");
+		Employee employee = new Employee();
+		Country ctry=new Country();
+		ctry.setCountryId(employeeUI.getCountryId());
+		ctry.setCname(employeeUI.getCname());
+
+		employee.setCountry(ctry);
+		
+		employee.setId(employeeUI.getId());
+		employee.setName(employeeUI.getName());
+		employee.setPhoneno(employeeUI.getPhoneno());
+		employee.setStatus(employeeUI.getStatus());
+		employee.setDepartmentit(employeeUI.getDepartmentit());
+		employee.setCreateddtm(employeeUI.getCreateddtm());
+		employee.setCreatedby(employeeUI.getCreatedby());
+		employee.setUpdateddtm(employeeUI.getUpdateddtm());
+		employee.setUpdatedby(employeeUI.getUpdatedby());
+
+		System.out.println("UI:" + employeeUI.getName());
+		System.out.println("emp:" + employee.getName());
+		System.out.println("UI" + employeeUI.getCname());
+		System.out.println("emp" + employee.getCountry().getCname());
+		return employee;
+	}
+}
